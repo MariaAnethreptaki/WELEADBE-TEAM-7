18720480a5a7c03728a02615e455804946c1a1af
POULAKI ANNA <anna.poulaki@hotmail.gr>
2023-12-03 20:39:15 +0200

    Add files via upload

---
+++ Store.java
@@ -0,0 +1,103 @@
+import java.util.List
+
+        // this class contains the information about the stores//
+public class Store {
+    private long storeId;
+    private String storeName;
+    private String storeLocation;
+    private double rate;
+    private List<String> menu;
+    private boolean status;
+    private String storeCategory;
+    private List<String> famousStores;
+}
+
+public Store(long storeId, String storeName,String storeLocation, Double rate, List<String>menu, boolean status, String storeCategory, List StringfamousStores) {
+    this.storeId = storeId;
+    this.storeName = storeName;
+    this.storeLocation = storeLocation;
+    this.rate = rate;
+    this.menu = menu;
+    this.status = status;
+    this.storeCategory = storeCategory;
+    this.famousStores = famousStores;
+}
+
+public long getStoreId(){
+    return storeId;
+}
+public void setstoreId(long storeId) {
+
+    this.storeId = storeId;
+}
+
+public String getStoreName(){
+
+    return storeName;
+}
+
+public void setStoreName(String storeName){
+
+    this.storeName = storeName;
+}
+
+public String getStoreLocation(){
+
+    return storeLocation;
+}
+
+public void setStoreLocation(String storeLocation){
+
+    this.storeLocation = storeLocation;
+}
+
+public double getRate(){
+
+    return rate;
+}
+
+public void setRate(double rate) {
+
+    this.rate = rate;
+}
+
+public List<String> getMenu(){
+
+    return menu;
+}
+
+public void setMenu(List<String> menu) {
+
+    this.menu = menu;
+}
+
+public boolean isStatus() {
+
+    return status;
+}
+
+public void setStatus(boolean status) {
+
+    this.status = status;
+}
+
+public String getStoreCategory() {
+
+    return storeCategory;
+}
+
+public void setStoreCategory(String storeCategory) {
+
+    this.storeCategory = storeCategory;
+}
+
+public List<String> getFamousStores() {
+
+    return famousStores;
+}
+
+public void setFamousStores(List<String> famousStores) {
+
+    this.famousStores = famousStores;
+}
+
