150ff2d12a5db6fcb63d2f831a2ff934deabac6d
MariaAnethreptaki <mariaanethreptaki1997@gmail.com>
2023-12-29 21:16:00 +0200

    P to p

--- app/src/main/EshopApplication.java
+++ app/src/main/EshopApplication.java
@@ -9,7 +9,9 @@ public class EshopApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(EshopApplication.class, args);
 		Account account = new Account("Sophia@","6937777777");
-		System.out.println(account);
+		account.toString();
+
+		System.out.println(account.getPassword());
 
 	}
 }
