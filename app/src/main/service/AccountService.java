fbb59c597c867b3778ef9e9e702dc00713422484
MariaAnethreptaki <mariaanethreptaki1997@gmail.com>
2024-01-04 18:47:14 +0200

    deletes ...

--- app/src/main/service/AccountService.java
+++ app/src/main/service/AccountService.java
@@ -1,6 +1,6 @@
 package main.service;
 
-import main.domain.Account; ...
+import main.domain.Account;
 
 public interface AccountService extends BaseService<Account,Long> {
     Account findByEmail(String email);
